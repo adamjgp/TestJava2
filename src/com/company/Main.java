8462ce99edb4e6abee2b00561ba0ddd10e8040ef
Adam <adamjgp@gmail.com>
2021-06-04 10:05:42 -0400

    added new printline

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("Hello, World!");
 	System.out.println("This is the Blackstaff.");
+	System.out.println("IntelliJ Branch");
     }
 }
 
